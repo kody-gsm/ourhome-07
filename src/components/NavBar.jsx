01663e313743e38154e5361c3f868c59e77a11e3
suy0ng <yunsuyong739@naver.com>
2023-04-18 13:38:58 +0900

    링크 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import "../styles/nav.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className="nav-area">
       <div className="nav-bar">
-        <img src={logo} className="logo-img" />
-        <button className="btn-qna-more">질문들 모아보기</button>
+        <Link to="/"><img src={logo} className="logo-img" /></Link>
+        <button className="btn-qna-more" onClick={() => window.location.href='/qna-answers'}>질문들 모아보기</button>
       </div>
       <hr />
     </div>
