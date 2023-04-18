849df98eecd040c5023e515b0a037e74aca96d14
kkh061101 <kkh061101@naver.com>
2023-04-18 13:32:54 +0900

    이동 버튼 추가 & 스크롤 바 커스텀

--- src/pages/Qnaa.js
+++ src/pages/Qnaa.js
@@ -8,30 +8,31 @@ export default function Qnaa() {
   function pushes(position) {
     const a = [
       {
-        title: "떡볶이가 먹고 싶어요.",
-        answer: '드세요.ㅋ'
-      }, {
-        title: "엄청 커다란 모기가 나의 발을 물었어요!",
-        answer: '약을 발라요.'
-      }, {
-        title: '당장 집에 달려가고 싶어요.',
-        answer: '조금만 참으세요.'
-      }, {
-        title: '놀라운 사실이 있는데 안 가르쳐줄거에요.',
-        answer: '그럼 알리지 마세요.'
-      }, {
-        title: '맙소사 당장 제 공중제비를 멈추십시오!',
-        answer: '병원을 가세요.'
-      }, {
         title: '난 이제 지쳤어요',
         answer: '땡벌!'
-      }, {
+      },
+      {
         title: '당신은 저에게 엄청난 것을 훔쳐갔습니다.',
         answer: '찾아가세요.'
       }, {
         title: '미안하다 이거 보여주려고 어그로 끌었다 나루토 사스케 싸움 ㄹㅇ실화냐?',
         answer: '실화 아닙니다.'
-      }
+      }, {
+        title: "저는 sw개발학과인데 iot에 분야를 생각하고 있어요.",
+        answer: '언제든지 환영입니다.'
+      }, {
+        title: "저는 웹 프로젝트를 주로 하고 싶어요.",
+        answer: '서브 프로젝트로 웹 프로젝트를 기획중입니다.'
+      }, {
+        title: '선배와 자주 이야기 하고 싶어요.',
+        answer: '여기 오면 많이 하게 됩니다.'
+      }, {
+        title: '놀라운 사실이 있는데 안 가르쳐줄거에요.',
+        answer: '그럼 알리주지 마세요.'
+      }, {
+        title: '맙소사 당장 제 공중제비를 멈추십시오!',
+        answer: '병원을 가세요.'
+      },
     ];
     let crntposi = a.length - (5 * position);
     let topush = [
@@ -55,7 +56,7 @@ export default function Qnaa() {
   }, []);
   return <div className="qnaa">
     <div className="header">
-      <img className="kodylogo" src={kodylogo} alt="kodylogo" />
+      <img className="kodylogo" src={kodylogo} alt="kodylogo" onClick={e => window.location.href = 'welcome'} />
       <a href="qna-questions"><button>Q&A 하러가기</button></a>
     </div>
     <hr />
