496acb1b30e019aa0cf69a69d77969503393a10e
GSMIOTjgh <s23054@gsm.hs.kr>
2024-03-28 22:33:13 +0900

    main

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,30 +1,8 @@
 import React from "react";
 
-function Answer({ isclicked }) {
+function Answer() {
   return (
-    <div className="question">
-      <div className="question-part">
-        <p className="attention-ment">
-          답변하는데 시간이 걸릴 수 있어요 조금만 기다려주세요 !
-        </p>
-        <div className="question-area">
-          <h1 className="upper">A</h1>
-          <br />
-          <span className="under">.</span>
-          <div className="answer-box">
-            <div
-              type="text"
-              className="answer-input-box answer"
-              disabled="disabled"
-            >
-              <span className={`${isclicked && "one"} dot`}>.</span>
-              <span className={`${isclicked && "two"} dot`}>.</span>
-              <span className={`${isclicked && "thr"} dot`}>.</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
   );
 }
 
--- src/pages/Qnaq.js
+++ src/pages/Qnaq.js
@@ -3,13 +3,8 @@ import NavBar from "../components/NavBar";
 import QuestionInput from "../components/QuestionInput";
 import Answer from "../components/Answer"
 
-export default function Qnaq({ url }) {
-  const [isclick, setIsclick] = useState(false);
+export default function Qnaq() {
   return (
-    <div className="qnaq">
-      <NavBar />
-      <QuestionInput isclicked={setIsclick} url={url} />
-      <Answer isclicked={isclick} />
-    </div>
+
   );
 }
