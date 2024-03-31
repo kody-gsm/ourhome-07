53f3af0b27684e1369e9c1e6acab8a0be49b60b3
GSMIOTjgh <s23054@gsm.hs.kr>
2024-03-31 23:24:48 +0900

    main

--- src/components/QuestionInput.jsx
+++ src/components/QuestionInput.jsx
@@ -4,7 +4,30 @@ import axios from "axios";
 
 function QuestionInput() {
   return(
-    <div></div>
+    <div className="question">
+      <div className="popup">
+
+      </div>
+      <div className="question-part">
+        <p className="attention-ment">
+          한 번 작성된 내용은 수정이 불가하니 신중히 작성해주세요 ㅠ_ㅠ
+        </p>
+        <div className="question-area">
+          <h1
+            className="upper"
+            >
+          Q .
+          </h1>
+          <div className="question-box">
+            <textarea
+              type="text"
+              placeholder="질문을 입력해주세요"
+              className="question-input-box" 
+              />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Qnaq.js
+++ src/pages/Qnaq.js
@@ -7,6 +7,7 @@ export default function Qnaq() {
   return (
     <div className="qnaq">
       <NavBar />
+      <QuestionInput />
       <Answer />
     </div>
   );
