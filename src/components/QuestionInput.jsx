827f0a8eaeaba1a9f4d7ce3018dad690c04e3705
GSMIOTjgh <s23054@gsm.hs.kr>
2024-04-01 22:26:01 +0900

    main

--- src/components/QuestionInput.jsx
+++ src/components/QuestionInput.jsx
@@ -1,30 +1,44 @@
-import React, { useEffect, useState } from "react";
-// import envelope from "../assets/envelope-solid.svg";
+import React, { useState } from "react";
 import axios from "axios";
 
 function QuestionInput() {
-  return(
-    <div className="question">
-      <div className="popup">
+  const [question, setQuestion] = useState("");
 
-      </div>
+  
+  const handleSubmit = async () => {
+    try {
+      
+      const response = await axios.post('url', { question });
+      console.log(response.data); 
+
+      
+      setQuestion("");
+    } catch (error) {
+      console.error('Error occurred while sending data:', error);
+      // 오류 처리
+    }
+  };
+
+  return (
+    <div className="question">
+      <div className="popup"></div>
       <div className="question-part">
         <p className="attention-ment">
           한 번 작성된 내용은 수정이 불가하니 신중히 작성해주세요 ㅠ_ㅠ
         </p>
         <div className="question-area">
-          <h1
-            className="upper"
-            >
-          Q . 
-          </h1>
+          <h1 className="upper">Q .</h1>
           <div className="question-box">
             <textarea
               type="text"
               placeholder="질문을 입력해주세요"
-              className="question-input-box" 
-              />
-              <button className="btn-answer">보내기</button>
+              className="question-input-box"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+            <button className="btn-answer" onClick={handleSubmit}>
+              보내기
+            </button>
           </div>
         </div>
       </div>
