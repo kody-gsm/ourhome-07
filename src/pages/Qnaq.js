05ccea7de08d57248ab4bfb8c1acf65f2d271f20
GSMIOTjgh <s23054@gsm.hs.kr>
2024-04-03 14:13:30 +0900

    main

--- src/pages/Qnaq.js
+++ src/pages/Qnaq.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import NavBar from "../components/NavBar";
 import QuestionInput from "../components/QuestionInput";
-import Answer from "../components/Answer"
+import Answer from "../components/Answer";
 
 export default function Qnaq() {
-
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    setIsAdmin(true);
+    axios.get("/api/user/isAdmin")
+      .then(response => {
+        setIsAdmin(response.data.isAdmin);
+      })
+      .catch(error => {
+        console.error("어드민 안 줘", error);
+      });
   }, []);
-
 
   return (
     <div className="qnaq">
