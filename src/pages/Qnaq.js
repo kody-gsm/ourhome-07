a8294655401a3bf80c16d475c1874d489614d3e3
GSMIOTjgh <s23054@gsm.hs.kr>
2024-04-08 23:17:47 +0900

    main

--- src/pages/Qnaq.js
+++ src/pages/Qnaq.js
@@ -11,7 +11,7 @@ export default function Qnaq() {
 
   const handleAdminLogin = async () => {
     try {
-      const response = await axios.post("https://4215-210-218-52-13.ngrok-free.app/kody/check", { password });
+      const response = await axios.post(process.env.REACT_APP_BACKEND_GET_CREATE_URL, { password });
   
       // 백엔드에서 받은 응답이 true이면 어드민으로 설정
       setIsAdmin(response.data);
@@ -27,19 +27,20 @@ export default function Qnaq() {
       <QuestionInput />
       <Answer />
 
-      <button className="Admin" onClick={handleAdminLogin}>관리자</button>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} // 입력값이 변경될 때마다 password 상태 업데이트
-        placeholder="비밀번호 입력"
-      />
-
       {isAdmin && (
         <Link to="/qna-answers-admin">
           <button className="Admin">어드민</button>
         </Link>
       )}
+      
+      <input className="AdminreInput"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} // 입력값이 변경될 때마다 password 상태 업데이트
+        placeholder="운영자 비번 적기"
+      />
+      <button className="AdminreButton" onClick={handleAdminLogin}>관리자</button>
+
     </div>
   );
 }
